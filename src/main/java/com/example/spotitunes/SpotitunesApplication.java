eee953811730932a63faca0152ba3e00771eba6b
jpdavis268 <72111292+ultranine@users.noreply.github.com>
2026-03-21 12:14:47 -0400

    Set up backend repo.

---
+++ src/main/java/com/example/spotitunes/SpotitunesApplication.java
@@ -0,0 +1,11 @@
+package com.example.spotitunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotitunesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpotitunesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/spotitunes/SpotitunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotitunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotitunesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
